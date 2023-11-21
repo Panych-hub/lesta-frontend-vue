2bdbf0eb3be7ce28f4d0231ae8f9cebc66843249
pavel.zamyatin <pavel.zamyatin@softcom.su>
2023-11-22 00:54:42 +0300

    fix cors bug

--- src/api/vehicle.ts
+++ src/api/vehicle.ts
@@ -6,9 +6,18 @@ import { baseURL } from "../constant/baseURL";
 export class VehicleApi {
   async get(): Promise<Vehicle[]> {
     return await axios
-      .post(baseURL + "glossary", {
-        query: QUERY_FOR_ALL_SHIPS,
-      })
+      .post(
+        baseURL + "glossary",
+        {
+          query: QUERY_FOR_ALL_SHIPS,
+        },
+        {
+          headers: {
+            "Access-Control-Allow-Origin": "*",
+            "Access-Control-Allow-Credentials": "true",
+          },
+        },
+      )
       .then((res) => res.data.data.vehicles)
       .catch((error) => console.error(error));
   }
